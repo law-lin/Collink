ba895899be685f61f035ec618bb53bbb8254aaf4
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 16:03:14 -0400

    added sample colleges

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 
-class CollegeStudent (ndb.Model):
+class CollegeStudent(ndb.Model):
     name = ndb.StringProperty()
     collegename = ndb.StringProperty()
     classyear = ndb.IntegerProperty()
