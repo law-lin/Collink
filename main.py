269bc33c5d990af6963c78bcf83ce363ab061d76
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 14:37:21 -0400

    background img

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import webapp2
+
+
 import jinja2
 import os
-from google.appengine.api import users
+
 import models
+
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -14,20 +17,11 @@ jinja_env = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_env.get_template("templates/index.html")
+        template = jinja_env.get_template("/templates/index.html")
         self.response.write(template.render())
-
-
-class AddEventPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_env.get_template("templates/addevent.html")
-        self.response.write(template.render())
-
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/addevent', AddEventPage),
 ], debug=True)
