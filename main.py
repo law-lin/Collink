b8700d3b040b50e6966b3749324a58db61963c09
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 07:24:41 -0700

    first main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import webapp2
+import jinja2
+import os
+
+import models
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template = jinja_env.get_template("Collink/index.html")
+        self.response.write(template.render())
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
