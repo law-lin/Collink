1276c2721402390c73e2f29ae3a0b5d290e99f2d
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 09:24:50 -0700

    Working Homepage and add eventa Page

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ jinja_env = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_env.get_template("Collink/templates/index.html")
+        template = jinja_env.get_template("templates/index.html")
         self.response.write(template.render())
 
 
 class AddEventPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        template = jinja_env.get_template("Collink/templates/addevent.html")
+        template = jinja_env.get_template("templates/addevent.html")
         self.response.write(template.render())
-    
+
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
-    ('/', AddEventPage),
+    ('/', MainPage),
+    ('/addevent', AddEventPage),
 ], debug=True)
