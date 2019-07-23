f6787b0d3197185c3a4d98ecfa9c87db606fa390
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 09:33:16 -0700

    Merging fix.

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import webapp2
 import jinja2
 import os
-<<<<<<< HEAD
-
-=======
 from google.appengine.api import users
 import models
->>>>>>> 1276c2721402390c73e2f29ae3a0b5d290e99f2d
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -18,11 +14,7 @@ jinja_env = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-<<<<<<< HEAD
-        template = jinja_env.get_template("/templates/index.html")
-=======
         template = jinja_env.get_template("templates/index.html")
->>>>>>> 1276c2721402390c73e2f29ae3a0b5d290e99f2d
         self.response.write(template.render())
 
 
