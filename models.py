fd5da9e35a02afee7454b080e0aaa7426b715fe8
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-25 17:34:21 -0400

    Minor fixes

--- models.py
+++ models.py
@@ -9,3 +9,8 @@ class Event(ndb.Model):
     event_des = ndb.TextProperty()
     host_email = ndb.StringProperty()
     event_type = ndb.StringProperty()
+    num_attendees = ndb.IntegerProperty(default=0)
+
+class User(ndb.Model):
+    email = ndb.StringProperty()
+    events = ndb.KeyProperty(repeated=True)
